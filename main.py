c8a7643849bed42e14844eb9b3d488cecc0e0e06
DJ (Salvo) <85457381+DJStompZone@users.noreply.github.com>
2021-12-05 13:17:05 -0800

    ReStyle, API encapsulation, failure info
    Site styling overhaul, encapsulated API calls, added more info to failure condition

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
-from flask import Flask, render_template, request, redirect
-from threading import Thread
+from flask import Flask, render_template, request
+import requests
+import base64
 
 app = Flask('')
+ciphey_api = 'https://pl8u5p7v00.execute-api.us-east-2.amazonaws.com/default/ciphey_lambda_api'
+sorrytxt = "Sorry, this text could not be decoded with this tool:"
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
   if request.method == 'POST':
-    return redirect(f"https://pl8u5p7v00.execute-api.us-east-2.amazonaws.com/default/ciphey_lambda_api?ctext={request.form.get('mysterytext')}")
+    c_result = ciphey_api_query(request.form.get('mysterytext'))
+    return render_template('result.html', cresult=c_result)
   elif request.method == 'GET':
     return render_template('index.html')
-  return render_template("index.html")
+  else: return "I... Literally don't even know how to respond to that." 
 
 @app.route('/<qry>')
 def redirect_to_api(qry):
-  return redirect(f"https://pl8u5p7v00.execute-api.us-east-2.amazonaws.com/default/ciphey_lambda_api?ctext={qry}")
+  c_result = ciphey_api_query(qry)
+  return c_result
 
+def ciphey_api_query(querytext) -> str:
+  query_bytes = querytext.encode('ascii')
+  base64_bytes = base64.b64encode(query_bytes)
+  base64_query = base64_bytes.decode('ascii')
+  try:
+    rslt = requests.get(f"{ciphey_api}?ctext={base64_query}")
+    if rslt.status_code == 200:
+      return rslt.text
+    else: return f"{sorrytxt} (raw: {querytext}) (as_b64: {base64_query})"
+  except Exception as e:
+    print(e)
+    return "Whoops! Sorry! An error occurred, please try again later."
 
 def run():
   app.run(host='0.0.0.0')
